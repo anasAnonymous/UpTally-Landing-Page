a68a9be4912d00a6d1d38f1dd733ae6eb161bc0c
zaidharissheikh <zaidsaigal2005@gmail.com>
2026-08-05 13:33:37 +0500

    feat: integrate Vercel Analytics into the main application component

--- uptally/src/App.jsx
+++ uptally/src/App.jsx
@@ -1,7 +1,7 @@
 import LandingPage from "./LandingPage";
-
+import { Analytics } from "@vercel/analytics/react"
 function App() {
-  return <LandingPage />;
+  return <><LandingPage /> <Analytics /></>;
 }
 
 export default App;
